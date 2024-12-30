8e58fe556668b85d5ab7bc6e8862d4315f6edcd2
sahsweta <sweta@xoniertechnologies.com>
2024-12-30 17:09:57 +0530

    ADD

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,6 @@
 "use client"
 import React,{useEffect} from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 const NotFound = () => {
   const router = useRouter()
