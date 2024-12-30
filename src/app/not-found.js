b58db0299566df69cce6918a9b65caf2b0a772c2
sahsweta <sweta@xoniertechnologies.com>
2024-12-30 16:52:55 +0530

    add

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,16 +3,13 @@ import React,{useEffect} from 'react'
 import { useRouter } from 'next/router'
 
 const NotFound = () => {
-
   const router = useRouter()
-
-   const handleBack = ()=> router.push('/')
-
+   
   return (
     <>
       <div className='min-h-screen w-full flex items-center justify-center bg-[#0C3246]'>
         <h1 className=''> Nothing Found Bro</h1>
-        <button type='button' onClick={handleBack}>Back to Home</button>
+        <button type='button' onClick={()=>router.push('/')}>Back to Home</button>
 
       </div>
     </>
